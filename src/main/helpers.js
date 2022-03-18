3b1854c0e99e83c3699a01111e45e08902087237
jantijn-enlyo <82444785+jantijn-enlyo@users.noreply.github.com>
2022-03-18 11:47:51 +0100

    nice helpers for parse caption

--- src/main/helpers.js
+++ src/main/helpers.js
@@ -26,6 +26,30 @@ function getUniqueListBy(arr, key) {
     return [...new Map(arr.map((item) => [item[key], item])).values()];
 }
 
+/**
+ * Parse caption
+ */
+function parseCaption(caption) {
+    if (caption.includes(' - ')) {
+        const words = caption.split(' - ');
+        return toTitleCase(words[words.length - 1]);
+    }
+    return toTitleCase(caption);
+}
+
+/**
+ * To title case
+ */
+function toTitleCase(phrase) {
+    return phrase
+        .toLowerCase()
+        .split(' ')
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+}
+
 module.exports.generateOutputName = generateOutputName;
 module.exports.getAppVersion = getAppVersion;
 module.exports.getUniqueListBy = getUniqueListBy;
+module.exports.parseCaption = parseCaption;
+module.exports.toTitleCase = toTitleCase;
