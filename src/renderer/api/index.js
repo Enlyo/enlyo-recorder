ee6c82cdb8495b536d3597383f033f084be9afca
Jantijn Kromwijk <jantijn@enlyo.com>
2022-04-11 09:44:40 +0200

    Update index.js

--- src/renderer/api/index.js
+++ src/renderer/api/index.js
@@ -1,24 +1,8 @@
-import axios from 'axios';
 import AuthService from './AuthService';
 import VideoService from './VideoService';
 
-import store from '../store';
-
-axios.defaults.baseURL = process.env.API_BASE;
-const $axios = axios.create({
-    baseURL: process.env.API_BASE || 'http://127.0.0.1:8000/',
-});
-$axios.interceptors.request.use((config) => {
-    const tokens = store.getters['auth/tokens'];
-
-    config.headers.Authorization = tokens.access ? `JWT ${tokens.access}` : '';
-    return config;
-});
-
-const ctx = { $axios };
-
-const auth = new AuthService('auth/', 'User', ctx);
-const video = new VideoService('videos/', 'Video', ctx);
+const auth = new AuthService('auth/', 'User');
+const video = new VideoService('videos/', 'Video');
 
 export default {
     auth,
